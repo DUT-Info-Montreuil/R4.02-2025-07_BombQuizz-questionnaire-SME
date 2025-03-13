1cf69f01fbf19662e4cb46feee75ee5119960828
Alexandre PHAM <pham.alexandre.05@gmail.com>
2025-03-13 14:05:38 +0100

    setup

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr07/jeuQuizz/services/interfacies/fichierServices.java
@@ -0,0 +1,14 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr07.jeuQuizz.services.interfacies;
+
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr07.jeuQuizz.entities.dto.QuestionnaireDTO;
+
+import java.io.File;
+import java.util.List;
+
+public class fichierServices {
+
+    public List<QuestionnaireDTO> fournirListQuestionnaire (File file) {
+
+    }
+
+}
